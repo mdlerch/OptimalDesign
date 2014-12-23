3ec72070571cf68e58ba18654d04ed1fb88fc8b9
Michael Lerch <mdlerch@gmail.com>
2014-12-22 23:04:43 -0700

    Add initialDesign in C++
    Makevars for linking to LAPACK for armadillo

---
+++ src/initialDesign.cpp
@@ -0,0 +1,32 @@
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// [[Rcpp::export]]
+arma::vec initDesign(const arma::mat& X)
+{
+    // get dimensions of candidate matrix
+    int nr = X.n_rows;
+    int nc = X.n_cols;
+
+    // svd decomposition (dc -- divide and conquer is fastest and default)
+    // use econ because we only need left side
+    // I believe econ also, be default gives nr by nc rather than nr by nr
+    arma::mat U, V;
+    arma::vec S;
+    arma::svd_econ(U, S, V, X, "left");
+
+    arma::vec l2norm(nr);
+
+    // Leverages are the root L^2-norm of U.
+    // We don't need actual leverages, just order so don't bother rooting.
+    int i = 0;
+    do {
+        // method when U is nr by nr
+        // l2norm(i) = arma::norm((U.row(i)).cols(0, nc - 1), 2);
+        l2norm(i) = arma::norm(U.row(i), 2);
+        i++;
+    } while(i < nr);
+
+    return l2norm;
+}
