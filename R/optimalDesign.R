3d88ae09043a0639d9827d4e7a11afd2f9a6b736
Michael Lerch <mdlerch@gmail.com>
2014-12-30 15:22:53 -0700

    Renamed main R file and added criteria argument

---
+++ R/optimalDesign.R
@@ -0,0 +1,22 @@
+optimalDesign <- function(formula, candidate, n, criteria = "D", iter = 100)
+{
+
+    # convert dataframe of design points to matrix of model points
+    candidateX <- model.matrix(formula, candidate)
+
+    # initial indices
+    current <- initDesign(candidateX, n)
+
+    criteriums <- c("D", "A", "G", "IV")
+    criteria <- match.arg(criteria, criteriums)
+
+    if (criteria == "D")
+    {
+        current <- fedorovcpp(candidateX, current, 1:nrow(candidate), iter)
+    } else {
+        stop("Only D criteria currently supported")
+    }
+
+    # R indices start with 1
+    candidate[current + 1, ]
+}
