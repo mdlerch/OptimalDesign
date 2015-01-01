14a9d996ba2019e84100ad286972d66127c246d6
joncheryl <prof.sherrill@gmail.com>
2014-12-31 22:19:52 -0500

    just a little more formatting...

--- R/utility.R
+++ R/utility.R
@@ -36,7 +36,8 @@ getEff <- function(formula, design, candidate)
 # rename these functions as you see fit (or the variables for that matter)
 #
 
-genFact <- function(n_levels, n_terms, i=n_terms){
+genFact <- function(n_levels, n_terms, i=n_terms)
+{
     # center levels to zero and get scaling like in AlgDesign
     levels <- scale(1:n_levels - n_levels/2 -.5, scale=(n_levels%%2/2+.5))
     this <- rep(rep(levels ,n_levels^(n_terms-i)), rep(n_levels^(i-1), n_levels^(n_terms-i+1)))
