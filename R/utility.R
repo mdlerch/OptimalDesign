a79e81227fa8a32c6758a99532aa8bf144183d24
joncheryl <prof.sherrill@gmail.com>
2014-12-31 22:17:58 -0500

    fixed tab/space issue

--- R/utility.R
+++ R/utility.R
@@ -4,29 +4,27 @@
 
 getEff <- function(formula, design, candidate)
 {
-	candidate <- model.matrix(formula, candidate)
-    X <- model.matrix(formula, design)
-	p <- ncol(X)
-	N <- nrow(X)
-		
-    # information matrix
-    infoMat <- solve(t(X)%*%X)
+    candidate <- model.matrix(formula, candidate/max(candidate))
+    X <- model.matrix(formula, design/max(candidate))
+    p <- ncol(X)
+    N <- nrow(X)
+	infoMat <- solve(t(X)%*%X)
 	
     # evaluate efficiencies
     dEff <- 100 / (det(infoMat)^(1/p) * N)
     aEff <- 100*p / (sum(diag(infoMat)) * N)
 
-	efficiencies <- list(dEff=dEff, aEff=aEff)
+    efficiencies <- list(dEff=dEff, aEff=aEff)
 	
-	# calculate iff candidate set provided
+    # calculate iff candidate set provided
     # I don't know about these to be honest
-	if(!missing(candidate)){
-		spv <- apply(N * t(candidate) * (infoMat%*%t(candidate)), 2, sum)
-		cat(spv)
-	 	gEff <- 100 * sqrt(p/N) / max(spv^.5)
-		iEff <- mean(spv^.5)
-		
-		efficiencies <- c(efficiencies, list(gEff=gEff, iEff=iEff))
+    if(!missing(candidate))
+    {
+        spv <- apply(N * t(candidate) * (infoMat%*%t(candidate)), 2, sum)
+        gEff <- 100 * sqrt(p/N) / max(spv^.5)
+        iEff <- mean(spv^.5)
+
+        efficiencies <- c(efficiencies, list(gEff=gEff, iEff=iEff))
     }
     
     efficiencies
@@ -40,19 +38,19 @@ getEff <- function(formula, design, candidate)
 
 genFact <- function(n_levels, n_terms, i=n_terms){
     # center levels to zero and get scaling like in AlgDesign
-	levels <- scale(1:n_levels - n_levels/2 -.5, scale=(n_levels%%2/2+.5))
-	this <- rep(rep(levels ,n_levels^(n_terms-i)), rep(n_levels^(i-1), n_levels^(n_terms-i+1)))
+    levels <- scale(1:n_levels - n_levels/2 -.5, scale=(n_levels%%2/2+.5))
+    this <- rep(rep(levels ,n_levels^(n_terms-i)), rep(n_levels^(i-1), n_levels^(n_terms-i+1)))
 
-	if(i>1) data.frame(unname(cbind( this, genFact(n_levels,n_terms,i-1) )))
-	else this
+    if(i>1) data.frame(unname(cbind( this, genFact(n_levels,n_terms,i-1) )))
+    else this
 }
 
 genBBD <- function()
 {
-	
+
 }
 
 genCCD <- function()
 {
-	
+
 }
