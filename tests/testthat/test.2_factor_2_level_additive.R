d7effc4d3b449948158d8408832352ec0b479243
Michael Lerch <mdlerch@gmail.com>
2015-01-29 12:33:20 -0700

    Add same tests as D for A

--- tests/testthat/test.2_factor_2_level_additive.R
+++ tests/testthat/test.2_factor_2_level_additive.R
@@ -24,12 +24,32 @@ get_nums <- function(mat, matcher)
     sort(out)
 }
 
-test_that("2^2 small grid", {
+test_that("2^2 small grid D", {
               expect_equal(get_nums(Xsmall, X2squared),
                            sort(as.numeric(rownames(optimalDesign(~x+y, Xsmall, 4, criterion = "D")))))
 })
 
-test_that("2^2 large grid", {
+test_that("2^2 large grid D", {
               expect_equal(get_nums(Xlarge, X2squared),
                            sort(as.numeric(rownames( optimalDesign(~x+y, Xlarge, 4, criterion = "D")))))
+})
+
+get_nums <- function(mat, matcher)
+{
+    matchrow <- function(r, mat)
+    {
+        intersect(which(r[1] == mat[ , 1]), which(r[2] == mat[ , 2]))
+    }
+    out <- apply(matcher, 1, function(x) matchrow(x, mat))
+    sort(out)
+}
+
+test_that("2^2 small grid A", {
+              expect_equal(get_nums(Xsmall, X2squared),
+                           sort(as.numeric(rownames(optimalDesign(~x+y, Xsmall, 4, criterion = "A")))))
+})
+
+test_that("2^2 large grid A", {
+              expect_equal(get_nums(Xlarge, X2squared),
+                           sort(as.numeric(rownames( optimalDesign(~x+y, Xlarge, 4, criterion = "A")))))
 })
