5e26b9e9e98332901feabc23a0b95a61438b1378
Michael Lerch <mdlerch@gmail.com>
2015-03-05 10:56:55 -0700

    Add D and A worker functions

--- src/genetic_real.cpp
+++ src/genetic_real.cpp
@@ -7,6 +7,9 @@ using namespace Rcpp;
 
 
 arma::ivec primedecomp(int);
+double get_delta_d(arma::mat, arma::mat, arma::mat);
+double get_delta_a(arma::mat, arma::mat, arma::mat);
+arma::vec delta_common(arma::mat, arma::mat, arma::mat);
 void grblend(arma::mat &, uint, arma::mat, arma::uvec, double);
 void grcreep(arma::mat &, uint, double size, double);
 void grmutat(arma::mat &, uint, double);
@@ -25,12 +28,31 @@ arma::mat opt_geneticrealcpp(arma::mat parents, int n, int iterations, arma::uve
     //  in c++!
 
     int M = parents.n_cols;
-
     arma::uvec second_parent;
+    arma::cube xpxinv(n, parents.n_rows / n, M);
+    arma::mat xpxi;
+    arma::mat X;
+    arma::vec Xv;
 
     arma::mat children = parents;
 
     int iter, child;
+
+    int i;
+
+    // hard code ~X1 + X2 for now.  Later use primedecomp to get actual design
+    // hard code D criterion.
+
+    // make x'x.inv
+
+    for (i = 0; i < M; ++i)
+    {
+        Xv = parents.col(i);
+        X = join_cols(Xv.subvec(0, n - 1), Xv.subvec(n, 2 * n - 1));
+        arma::inv(xpxi, X.t() * X);
+        xpxinv.slice(i) = xpxi;
+    }
+
 
     for (iter=0; iter<M; ++iter)
     {
@@ -44,6 +66,9 @@ arma::mat opt_geneticrealcpp(arma::mat parents, int n, int iterations, arma::uve
             grcreep(children, child, .3, 0);
             grmutat(children, child, .5);
         }
+
+
+
 
     }
 
@@ -125,4 +150,77 @@ arma::ivec primedecomp(int num)
 
     primes.resize(idx);
     return primes;
+}
+
+arma::vec delta_common(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
+{
+    // Fedorov values as 1 by 1 matrices
+    arma::mat diim(1, 1), doom(1, 1), diom(1, 1);
+    // Fedorov values as doubles
+    double dii, doo, dio;
+
+    double delta;
+
+    arma::vec common;
+
+    diim = row_in * xpxinv * row_in.t();
+    doom = row_out * xpxinv * row_out.t();
+    diom = row_in * xpxinv * row_out.t();
+
+    // Fedorov values as double
+    dii = diim(0, 0);
+    doo = doom(0, 0);
+    dio = diom(0, 0);
+
+    delta = ((1 + dii) * (1 - doo) + dio * dio - 1);
+
+    common(0) = delta;
+    common(1) = dii;
+    common(2) = doo;
+    common(3) = dio;
+
+    return common;
+}
+
+double get_delta_d(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
+{
+    arma::vec common;
+
+    common = delta_common(xpxinv, row_in, row_out);
+
+    return common(0);
+}
+
+double get_delta_a(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
+{
+    // fedorov A phi values as matrices
+    arma::mat phiiim, phioom, phiiom, phioim;
+    // fedorov A phi values as double
+    double phiii, phioo, phiio, phioi;
+
+    double delta;
+
+    arma::vec common;
+
+    common = delta_common(xpxinv, row_in, row_out);
+
+    double delta_d = common(0);
+    double dii = common(1);
+    double dio = common(2);
+    double doo = common(3);
+
+    phiiim = row_in * xpxinv * xpxinv * row_in.t();
+    phiiom = row_out * xpxinv * xpxinv * row_in.t();
+    phioim = row_in * xpxinv * xpxinv * row_out.t();
+    phioom = row_out * xpxinv * xpxinv * row_out.t();
+
+    // convert phi values to double
+    phiii = phiiim(0, 0);
+    phiio = phiiom(0, 0);
+    phioi = phioim(0, 0);
+    phioo = phioom(0, 0);
+
+    delta = ( (1 - dii) * phioo + dio * (phiio + phioi) - (1 + doo) * phiii ) / (1 + delta_d);
+
+    return delta;
 }
