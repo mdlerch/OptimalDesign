0466809a84fb78a59e0044b55e6ef5398f01c9a0
Michael Lerch <mdlerch@gmail.com>
2015-04-01 07:24:23 -0600

    Add efficiency tests

--- tests/testthat/test.additive.crit.R
+++ tests/testthat/test.additive.crit.R
@@ -9,34 +9,34 @@ names(X) <- c("X1", "X2")
 formula <- ~X1 + X2
 
 test_that("D > 99%", {
-              expect_more_than(
-                           getEff(formula,
-                                  optimalDesign(formula, X, 4, "D", 1000000),
-                                  criteria = "D")$D,
-                           99)
+    expect_more_than(
+        getEff(formula,
+               optimalDesign(formula, X, 4, "D", 1000000),
+               criteria = "D")$D,
+        99)
 })
 test_that("A > 73", {
-              expect_more_than(
-                           getEff(formula,
-                                  optimalDesign(formula, X, 4, "A", 1000000),
-                                  criteria = "A")$A,
-                           73)
+    expect_more_than(
+        getEff(formula,
+               optimalDesign(formula, X, 4, "A", 1000000),
+               criteria = "A")$A,
+    73)
 })
 
 test_that("I > 1", {
-              expect_more_than(
-                           getEff(formula,
-                                  optimalDesign(formula, X, 4, "I", 1000000),
-                                  evaluation = X,
-                                  criteria = "I")$I,
-                           1)
+    expect_more_than(
+       getEff(formula,
+              optimalDesign(formula, X, 4, "I", 1000000),
+              evaluation = X,
+              criteria = "I")$I,
+    1)
 })
 
 test_that("G > 49.99", {
-              expect_more_than(
-                           getEff(formula,
-                                  optimalDesign(formula, X, 4, "D", 1000000),
-                                  evaluation = X,
-                                  criteria = "G")$G,
-                           49.99)
+    expect_more_than(
+        getEff(formula,
+               optimalDesign(formula, X, 4, "D", 1000000),
+               evaluation = X,
+               criteria = "G")$G,
+    49.99)
 })
