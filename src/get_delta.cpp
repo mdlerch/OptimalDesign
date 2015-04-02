a19d0809d33d278810ebaab6f5d3ecd9f1a01175
Michael Lerch <mdlerch@gmail.com>
2015-04-01 21:33:17 -0600

    Clean up get_delta

--- src/get_delta.cpp
+++ src/get_delta.cpp
@@ -5,29 +5,18 @@ using namespace Rcpp;
 
 arma::vec delta_common(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
 {
-    // Fedorov values as 1 by 1 matrices
-    arma::mat diim(1, 1), doom(1, 1), diom(1, 1);
-    // Fedorov values as doubles
-    double dii, doo, dio;
-
-    double delta;
-
-    arma::vec common(4);
-
-
-    diim = row_in * xpxinv * row_in.t();
-    doom = row_out * xpxinv * row_out.t();
-    diom = row_in * xpxinv * row_out.t();
-
+    arma::mat diim = row_in * xpxinv * row_in.t();
+    arma::mat doom = row_out * xpxinv * row_out.t();
+    arma::mat diom = row_in * xpxinv * row_out.t();
 
     // Fedorov values as double
-    dii = diim(0, 0);
-    doo = doom(0, 0);
-    dio = diom(0, 0);
+    double dii = diim(0, 0);
+    double doo = doom(0, 0);
+    double dio = diom(0, 0);
 
-    delta = ((1 + dii) * (1 - doo) + dio * dio - 1);
+    double delta = ((1 + dii) * (1 - doo) + dio * dio - 1);
 
-
+    arma::vec common(4);
     common(0) = delta;
     common(1) = dii;
     common(2) = doo;
@@ -38,104 +27,75 @@ arma::vec delta_common(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
 
 double get_delta_d(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
 {
-    arma::vec common;
-
-    common = delta_common(xpxinv, row_in, row_out);
-
+    arma::vec common = delta_common(xpxinv, row_in, row_out);
     return common(0);
 }
 
 double get_delta_a(arma::mat xpxinv, arma::mat row_in, arma::mat row_out)
 {
-    // fedorov A phi values as matrices
-    arma::mat phiiim, phioom, phiiom, phioim;
-    // fedorov A phi values as double
-    double phiii, phioo, phiio, phioi;
-
-    double delta;
-
-    arma::vec common;
-
-    common = delta_common(xpxinv, row_in, row_out);
+    arma::vec common = delta_common(xpxinv, row_in, row_out);
 
     double delta_d = common(0);
     double dii = common(1);
     double dio = common(2);
     double doo = common(3);
 
-    phiiim = row_in * xpxinv * xpxinv * row_in.t();
-    phiiom = row_out * xpxinv * xpxinv * row_in.t();
-    phioim = row_in * xpxinv * xpxinv * row_out.t();
-    phioom = row_out * xpxinv * xpxinv * row_out.t();
+    arma::mat phiiim = row_in * xpxinv * xpxinv * row_in.t();
+    arma::mat phiiom = row_out * xpxinv * xpxinv * row_in.t();
+    arma::mat phioim = row_in * xpxinv * xpxinv * row_out.t();
+    arma::mat phioom = row_out * xpxinv * xpxinv * row_out.t();
 
     // convert phi values to double
-    phiii = phiiim(0, 0);
-    phiio = phiiom(0, 0);
-    phioi = phioim(0, 0);
-    phioo = phioom(0, 0);
+    double phiii = phiiim(0, 0);
+    double phiio = phiiom(0, 0);
+    double phioi = phioim(0, 0);
+    double phioo = phioom(0, 0);
 
-    delta = ( (1 - doo) * phiii + dio * (phiio + phioi) - (1 + dii) * phioo ) / (1 + delta_d);
+    double delta = ( (1 - doo) * phiii + dio * (phiio + phioi) - (1 + dii) * phioo ) / (1 + delta_d);
 
     return delta;
 }
 
 double get_delta_g(double g_crit_old, arma::mat X, arma::mat U_can)
 {
-    double delta;
-
-    arma::mat Dinv, svd_thing;
+    arma::vec s;
     arma::mat U, V;
-
-    arma::vec s, leverages;
-
-    double g_crit;
-
     arma::svd_econ(U, s, V, X, "right");
-
     s = 1 / s;
 
-    Dinv= arma::diagmat(s);
+    arma::mat Dinv = arma::diagmat(s);
 
-    svd_thing = U_can * V * Dinv;
+    arma::mat svd_thing = U_can * V * Dinv;
     svd_thing = svd_thing % svd_thing;
-    leverages = arma::sum(svd_thing, 1);
-    g_crit = leverages.max();
+    arma::vec leverages = arma::sum(svd_thing, 1);
+    double g_crit = leverages.max();
 
-    delta = g_crit - g_crit_old;
-    
+    double delta = g_crit - g_crit_old;
+
     return delta;
 }
 
 double get_delta_i(arma::mat xpxinv, arma::mat row_in, arma::mat row_out, arma::mat B)
 {
-    // fedorov I phi values as matrices
-    arma::mat phiiim, phioom, phiiom, phioim;
-    // fedorov I phi values as double
-    double phiii, phioo, phiio, phioi;
-
-    double delta;
-    
-    arma::vec common;
-
-    common = delta_common(xpxinv, row_in, row_out);
+    arma::vec common = delta_common(xpxinv, row_in, row_out);
 
     double delta_d = common(0);
     double dii = common(1);
     double dio = common(2);
     double doo = common(3);
 
-    phiiim = row_in * xpxinv * B * xpxinv * row_in.t();
-    phiiom = row_out * xpxinv * B * xpxinv * row_in.t();
-    phioim = row_in * xpxinv * B * xpxinv * row_out.t();
-    phioom = row_out * xpxinv * B * xpxinv * row_out.t();
+    arma::mat phiiim = row_in * xpxinv * B * xpxinv * row_in.t();
+    arma::mat phiiom = row_out * xpxinv * B * xpxinv * row_in.t();
+    arma::mat phioim = row_in * xpxinv * B * xpxinv * row_out.t();
+    arma::mat phioom = row_out * xpxinv * B * xpxinv * row_out.t();
 
     // convert phi values to double
-    phiii = phiiim(0, 0);
-    phiio = phiiom(0, 0);
-    phioi = phioim(0, 0);
-    phioo = phioom(0, 0);
+    double phiii = phiiim(0, 0);
+    double phiio = phiiom(0, 0);
+    double phioi = phioim(0, 0);
+    double phioo = phioom(0, 0);
 
-    delta = ( (1 - doo) * phiii + dio * (phiio + phioi) - (1 + dii) * phioo ) / (1 + delta_d);
+    double delta = ( (1 - doo) * phiii + dio * (phiio + phioi) - (1 + dii) * phioo ) / (1 + delta_d);
 
     return delta;
 }
